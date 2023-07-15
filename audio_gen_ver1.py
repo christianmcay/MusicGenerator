4fcb21a686dc6bf499d3e13d0f63045897dfd236
Cabrera Christian <73971223+christianmcay@users.noreply.github.com>
2023-07-14 23:21:26 -0400

    Add files via upload

---
+++ audio_gen_ver1.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import wave
+import struct
+import random
+from scipy.io.wavfile import write
+
+class AudioProcessing:
+
+    def __init__(self, sample_rate, frequency, duration, amplitude):
+        self.sample_rate = sample_rate
+        self.frequency = frequency
+        self.duration = duration
+        self.amplitude = amplitude 
+    
+    def generate_sine_wave(self):
+        num_samples = int(self.sample_rate * self.duration)
+        samples = np.arange(num_samples)
+        waveform = self.amplitude * np.sin(2 * np.pi * self.frequency * samples / self.sample_rate)
+        return waveform.astype(np.int16)
+    
+    def generate_square_wave(self):
+        num_samples = int(self.sample_rate * self.duration)
+        samples = np.arange(num_samples)
+        w = 2 * np.pi * self.frequency
+        square_wave = 0
+        for index in range(1, 300, 2):  
+            square_wave += (1/index) * np.sin(index * w * (samples / self.sample_rate))
+        square_wave = self.amplitude * (4/np.pi) * square_wave
+        return square_wave.astype(np.int16)
+
+    def generate_triangle_wave(self):
+        num_samples = int(self.sample_rate * self.duration)
+        samples = np.arange(num_samples)
+        w = 2 * np.pi * self.frequency
+        triangle_wave = 0
+        for index in range(1, 300, 1):  
+            triangle_wave += (((-1)**index)/((2*index-1)**2)) * np.sin((2* index - 1) * w * (samples / self.sample_rate))
+        triangle_wave = self.amplitude * (-1) * (8/(np.pi**2)) * triangle_wave
+        return triangle_wave.astype(np.int16)
+
+    def generate_sawtooth_wave(self):
+        num_samples = int(self.sample_rate * self.duration)
+        samples = np.arange(num_samples)
+        w = 2 * np.pi * self.frequency
+        sawtooth_wave = 0
+        for index in range(1, 300, 1):
+            sawtooth_wave += ((-1)**index / index) * np.sin(w * index * (samples / self.sample_rate))
+        sawtooth_wave = self.amplitude *(-2/np.pi) * sawtooth_wave
+        return sawtooth_wave.astype(np.int16)
+
+    def plot_waveform(self, waveform):
+        duration = len(waveform) / self.sample_rate
+        time = np.linspace(0, self.duration, len(waveform))
+        plt.figure()
+        plt.plot(time, waveform)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.title('Waveform')
+        plt.show()
+
+    def byte_convert(self, waveform):
+        import numpy as np
+
+        # Create a NumPy array
+        arr = np.array([1, 2, 3, 4, 5], dtype=np.uint8)
+
+        # Convert NumPy array to bytes
+        bytes_data = waveform.tobytes()
+
+        # Print the bytes data
+        print(bytes_data)
+
+    #deprecated in favor of using scipy io wave functions
+
+    # def save_to_wav(self, filename, waveform):
+    #     try:
+    #         with wave.open(filename, 'w') as wav_file:
+    #             wav_file.setnchannels(1)
+    #             wav_file.setsampwidth(2)
+    #             wav_file.setframerate(self.sample_rate)
+    #             wav_file.setcomptype('NONE', 'not compressed')
+
+    #             for sample in waveform:
+    #                 packed_sample = struct.pack('h', sample)
+    #                 wav_file.writeframes(packed_sample)
+    #     except Exception as e:
+    #         print(f"Failed to save waveform to file: {e}")
+
+    # def read_wav_file(self, filename):
+    #     try:
+    #         with wave.open(filename, 'r') as wav_file:
+    #             num_frames = wav_file.getnframes()
+    #             wav_data = wav_file.readframes(num_frames)
+    #             waveform = np.frombuffer(wav_data, dtype=np.int16)
+    #         return waveform
+    #     except Exception as e:
+    #         print(f"Failed to read waveform from file: {e}")
+    #         return None
+
+note_frequencies = np.array([[27.50], [29.14], [30.87],
+                            [32.70], [34.65], [36.71], 
+                            [38.89], [41.20], [43.65], 
+                            [46.25], [49.00], [51.91],
+                            [55.00], [58.27], [61.74],
+                            [65.41], [69.30], [73.42], 
+                            [77.78], [82.41], [87.31], 
+                            [92.50], [98.00], [103.83],
+                            [110.00], [116.54], [123.47],
+                            [130.81], [138.59], [146.83], 
+                            [155.56], [164.81], [174.61], 
+                            [185.00], [196.00], [207.65],
+                            [220.00], [233.08], [246.94],
+                            [261.63], [277.18], [293.66], 
+                            [311.13], [329.63], [349.23], 
+                            [369.99], [392.00], [415.30],
+                            [440.00], [466.16], [493.88],
+                            [523.25], [554.37], [587.33], 
+                            [622.25], [659.25], [698.46], 
+                            [739.99], [783.99], [830.61],
+                            [880.00], [932.33], [987.77],
+                            [1046.50], [1108.73], [1174.66], 
+                            [1244.51], [1318.51], [1396.91], 
+                            [1479.98], [1567.98], [1661.22],
+                            [1760.00], [1864.66], [1975.53],
+                            [2093.00], [2217.46], [2349.32], 
+                            [2489.02], [2637.02], [2793.83], 
+                            [2959.96], [3135.96], [3322.44],
+                            [3520.00], [3729.31], [3951.07],
+                            [4186.01]
+                            ])
+
+index = 0
+
+noteStart = random.choice(note_frequencies)   #random freq from note frequency array 
+for index in range(0, len(note_frequencies)): #start at index 0 and keep going until our random note is found
+    if noteStart == note_frequencies[index]:
+        indexFreq = index                     #if note found set indexfreq var equal to index value found
+    index += 1
+print(f"start freq {note_frequencies[indexFreq]}")
+
+
+index1 = 0             
+index2 = 0         
+indexFreqMajor = indexFreq * 1
+indexFreqMinor = indexFreq * 1
+
+majorScale = [2,2,1,2,2,2,1,0]
+print("Major Scale")
+major_waveform = np.array([], dtype=np.int16)
+for index1 in range(0,8):
+    if indexFreqMajor >= 76:
+        exit()
+    else:
+        freq = note_frequencies[indexFreqMajor]
+        audio = AudioProcessing(sample_rate=44100, frequency=freq, duration=1 ,amplitude=25000)
+        sq_wave = audio.generate_sine_wave()
+        #print(note_frequencies[indexFreqMajor])
+        major_waveform = np.append(major_waveform, sq_wave)
+        indexFreqMajor = indexFreqMajor + majorScale[index1]  #increase start freq note index by scale value jumps
+
+
+minorScale = [2,1,2,2,1,2,2,0]
+print("\nMinor Scale")
+minor_waveform = np.array([], dtype=np.int16)
+for index2 in range(0,8):
+    if indexFreqMinor >= 76:
+        exit()
+    else:                        #range of scale value jumps 
+        freq = note_frequencies[indexFreqMinor]
+        audio2 = AudioProcessing(sample_rate=44100, frequency=freq, duration=1 ,amplitude=25000)
+        sq_wave = audio2.generate_sine_wave()
+        #print(note_frequencies[indexFreqMajor])
+        minor_waveform = np.append(minor_waveform, sq_wave)
+        indexFreqMinor = indexFreqMinor + minorScale[index2]  #increase start freq note index by scale value jumps
+
+write(f'{note_frequencies[indexFreq]}_major_scale.wav', 44100, major_waveform)
+write(f'{note_frequencies[indexFreq]}_minor_scale.wav', 44100, minor_waveform)
+
